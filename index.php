42c18fd8a7930e2c2cc81822e2fd0ba2d7f7d150
Vinicius da Silva Nogueira <msn-dovinicius@hotmail.com>
2022-10-19 13:42:41 -0300

    Inicializando projeto de estoque de papelaria.

--- index.php
+++ index.php
@@ -6,22 +6,28 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>/css/style.css">
+	<!--fonts-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https: //fonts.googleapis.com/css2? família= Rubik:wght@300;400 & display=swap" rel="stylesheet">
 	<title>Estoque Papelaria</title>
 </head>
 <body>
+	<div class="center">
+	<header>
+		<h1>ESTOQUE DE PAPELARIA</h1>
+	</header>
 	<section>
-		Adicionar produto ao estoque:
-		<button>ADICIONAR</button>
+		<?php
 
-		<table>
-			<tr>
-				<th>PRODUTO</th>
-			</tr>
-			<tr>
-				<td>LÁPIS</td>
-			</tr>
-		
-		</table>
+			$url == $_GET[""];
+
+			if($url ==  $_GET[INCLUDE_PATH]){
+				include("pages/menu.php");
+			}
+
+		?>
 	</section>
+</div><!--center-->
 </body>
 </html>
---
+++ pages/menu.php
@@ -0,0 +1,23 @@
+	<p class="adicionar_txt">Adicionar produto ao estoque:</p>
+		<a class="adicionar_btn" href="">ADICIONAR</a>
+
+		<table>
+			<tr>
+				<th>PRODUTO</th>
+				<th>UNIDADE</th>
+				<th>VALOR DO CUSTO</th>
+				<th>FORNECEDOR</th>
+				<th>GRUPO</th>
+				<th></th>
+				<th></th>
+			</tr>
+			<tr>
+				<td>LÁPIS</td>
+				<td>23</td>
+				<td>5,00</td>
+				<td>GENERICO</td>
+				<td>DIVERSOS</td>
+				<td><a class="editar_btn"href="">EDITAR</a></td>
+				<td><a class="deletar_btn" href="">DELETAR</a></td>
+			</tr>		
+		</table>
