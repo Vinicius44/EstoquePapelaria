b2a0129e25c30f0bcaad1c041404359cc65aa754
Vinicius da Silva Nogueira <msn-dovinicius@hotmail.com>
2022-10-18 23:54:26 -0300

    Iniciando um estoque de papelaria

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+	
+	$autoload = function($class){
+	
+		include('classes/'.$class.'.php');
+	};
+
+	spl_autoload_register($autoload);
+	define("INCLUDE_PATH", "http://localhost/Portf%c3%b3lio/Back-End/Projeto%20Papelaria/");
+
+
+
+	//conectar com o banco de dados
+	define("HOST", 'localhost');
+	define("USER", "root");
+	define("PASSWORD", "");
+	define("DATABASE","projeto_01");
+
+	
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include("config.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>/css/style.css">
+	<title>Estoque Papelaria</title>
+</head>
+<body>
+	<section>
+		Adicionar produto ao estoque:
+		<button>ADICIONAR</button>
+
+		<table>
+			<tr>
+				<th>PRODUTO</th>
+			</tr>
+			<tr>
+				<td>LÁPIS</td>
+			</tr>
+		
+		</table>
+	</section>
+</body>
+</html>
